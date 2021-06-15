b6f014a8edb8b39a218d381a03d0f5cb0954372e
Eliaztray <944126009@qq.com>
2021-06-15 14:48:22 +0800

    feat: add bilibili OAuth

--- packages/third-party/test/example.spec.ts
+++ packages/third-party/test/example.spec.ts
@@ -6,7 +6,7 @@ import Lingxi from '../example/lingxi.json'
 describe('e2e: full example', () => {
     const { page, text, click, value } = setupPuppeteer()
 
-    beforeEach(() => {
+    beforeEach(async () => {
         // TODO: 启动 serve.
     }, 2000)
 
@@ -55,6 +55,46 @@ describe('e2e: full example', () => {
         async () => {
             await page().goto('http://localhost:5000/example/lingxi?a=1')
             expect(await text('#lingxi')).toBe('lingxi login')
+
+            // 跳转三方授权登录页面
+            const [response] = await Promise.all([page().waitForNavigation(), click('#lingxi')])
+            if (response.ok()) {
+                // 选中密码登录, 位于第二个 tab
+                await page().click('.ant-tabs-tab:nth-child(2)')
+
+                // 自动键入登录账号, 这个页面有两个 id mobile
+                await page().focus('.ant-form:nth-child(2) #mobile')
+                await page().keyboard.type(Lingxi.account)
+                expect(await value('.ant-form:nth-child(2) #mobile')).toBe(Lingxi.account)
+
+                await page().focus('#password')
+                await page().keyboard.type(Lingxi.password)
+                expect(await value('#password')).toBe(Lingxi.password)
+                // 模拟表单提交
+                await click('.ant-form:nth-child(2) button[type="submit"]')
+
+                // 等待重定向回 localhost
+                await page().waitForSelector('#token')
+                // 等待请求完成
+                await timeout(2000)
+                expect((await text('#token'))?.length).toBe(40)
+
+                const str = await page().evaluate(() => window.localStorage.getItem('@ali-ieu/sdk:access_token:lingxi'))
+                const data = JSON.parse(str ?? 'null')
+                expect(data.value.accessToken).toBe(await text('#token'))
+                expect(data.value.thirdPartyChannel).toBe('lingxi')
+
+                expect(await page().evaluate(() => location.href)).toBe('http://localhost:5000/example/lingxi?a=1')
+            }
+        },
+        E2E_TIMEOUT,
+    )
+
+    it(
+        'test bilibili login',
+        async () => {
+            await page().goto('http://localhost:5000/example/bilibili?a=1')
+            expect(await text('#bilibili')).toBe('bilibili login')
 
             // 跳转三方授权登录页面
             const [response] = await Promise.all([page().waitForNavigation(), click('#lingxi')])
