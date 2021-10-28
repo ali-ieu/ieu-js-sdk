e17167ff41510cc4a4a2ea78f648627e76263358
agneslee <lige.li@alibaba-inc.com>
2021-10-28 17:42:48 +0800

    feat(big-eye): add big-eye monitor

---
+++ packages/big-eye/test/big-eye.spec.ts
@@ -0,0 +1,16 @@
+import { BigEye, BrowserLogger } from '../src'
+
+describe('big-eye', () => {
+    it('test event success', () => {
+        BigEye({
+            targetId: 'app',
+            envFlag: 'prod',
+            loggerConfig: {
+                release: '1.0.0',
+                pid: 'dsv9zcel92@d92a991af7669c9',
+                name: 'test',
+            },
+        })
+        expect(BrowserLogger).not.toBe(undefined)
+    })
+})
